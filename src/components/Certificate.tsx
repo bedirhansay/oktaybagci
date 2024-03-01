ae8320c18462c558c5f21cb1ec4a14879c527ddd
bedirhansay <bedirhan.sayy@gmail.com>
2024-03-01 18:31:00 +0300

    certificate

--- src/components/Certificate.tsx
+++ src/components/Certificate.tsx
@@ -1,5 +1,50 @@
+"use client";
 import React from "react";
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Autoplay,
+} from "swiper/modules";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+import Image from "next/image";
+import { certificate, sliders } from "@/constant/WorkingArea";
 
 export const Certificate = () => {
-  return <div>Certificate</div>;
+  return (
+    <div className="bg-customGray pt-8 pb-16">
+      <h2 className="text-2xl uppercase font-bold text-center mt-4 text-customYellow">
+        Certifacate
+      </h2>
+      <p className="text-4xl mb-16 text-center text-white ">Our Adequates</p>
+
+      <div className="mb-16 mt-4 clamp">
+        <Swiper
+          slidesPerView="auto"
+          className="w-full"
+          modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+          navigation
+          loop
+          speed={1800}
+          freeMode
+          autoplay
+          scrollbar={{ draggable: true }}
+        >
+          {certificate.map((item, idx) => (
+            <SwiperSlide className="m-2 !w-[400px]" key={idx + "k"}>
+              <Image width={400} loading="lazy" src={item} alt="" />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
 };
--- src/constant/WorkingArea.ts
+++ src/constant/WorkingArea.ts
@@ -18,6 +18,15 @@ import swarz from "../../public/markalar/svarz.png";
 import wella from "../../public/markalar/wella.png";
 import tony from "../../public/markalar/tony.png";
 
+import c1 from "../../public/certificate/c1.jpg";
+import c2 from "../../public/certificate/c2.jpg";
+import c3 from "../../public/certificate/c3.jpg";
+import c4 from "../../public/certificate/c4.jpg";
+import c5 from "../../public/certificate/c5.jpg";
+import c6 from "../../public/certificate/c6.jpg";
+import c7 from "../../public/certificate/c7.jpg";
+import c8 from "../../public/certificate/c8.jpg";
+
 // brands
 
 export const workingArea = [
@@ -39,6 +48,7 @@ export const workingArea = [
 ];
 
 export const sliders = [slider1, slider2, slider6, slider4, slider3, slider5];
+export const certificate = [c1, c2, c3, c4, c5, c6, c7, c8];
 
 export const brands = [
   keraste,
