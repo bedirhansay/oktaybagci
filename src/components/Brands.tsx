19e976f8e017d458fd00f3977b43a93d145ac63d
bedirhansay <bedirhan.sayy@gmail.com>
2024-03-05 01:23:09 +0300

    firm about

--- src/components/Brands.tsx
+++ src/components/Brands.tsx
@@ -10,10 +10,10 @@ export const Brands = () => {
     <div className="flex flex-col gap-12 bg-gray-50 p-4 shadow-md py-12 ">
       <h2 className="text-3xl text-center">{t("brandsWork")}</h2>
 
-      <div className="flex flex-wrap justify-between gap-12 brands__item ">
+      <div className="flex max-w-5xl mx-auto flex-wrap justify-between gap-12 brands__item ">
         {brands.map((item, id) => (
           <Image
-            className="w-16 "
+            className="!w-44"
             alt=""
             key={id + "item"}
             objectFit="contain"
--- src/components/Firm.tsx
+++ src/components/Firm.tsx
@@ -1,31 +1,14 @@
+"use client";
+import { useTranslations } from "next-intl";
 import React from "react";
 
 export const Firm = () => {
+  const t = useTranslations("aboutUs");
   return (
-    <div className="max-w-5xl mx-auto px-4">
-      <h2 className="font-semibold text-2xl">
-        Beeld is het beeld dat we achterlaten in de hoofden van anderen.
-      </h2>
-      <p className="text-justify tracking-wide leading-relaxed">
-        Lorem ipsum dolor sit amet consectetur. Integer mi tortor magna feugiat
-        viverra consectetur. Porta volutpat interdum tellus cursus dignissim
-        nunc at enim ultrices. Sagittis integer netus morbi cras nisl diam. Et
-        egestas elementum volutpat eget id rhoncus cras hendrerit. Id
-        pellentesque egestas euismod leo sodales metus consequat dolor nibh. Leo
-        at sollicitudin egestas turpis congue ut. Ornare morbi lacus amet
-        ultrices urna. Commodo ultrices sed quis elit luctus mattis nunc vel.
-        Dui mollis ac nulla sagittis sociis est massa. Et dui vel eu dui.
-        Sagittis id dignissim at est. Molestie odio vitae egestas egestas mi
-        lobortis neque mattis auctor. In donec elementum viverra in suspendisse
-        semper senectus commodo. Consequat at congue tempus tempus. Eget et
-        curabitur elit ullamcorper luctus. Bibendum enim ut nunc ac laoreet sed
-        ullamcorper nunc. Volutpat amet gravida facilisis et condimentum egestas
-        neque neque. Tellus feugiat velit eu enim fusce venenatis aliquam
-        ridiculus pharetra. Auctor pellentesque vestibulum urna vitae feugiat
-        lacus aliquam diam ac. Nulla facilisi vestibulum leo duis. Placerat erat
-        faucibus convallis etiam mi nam ut augue viverra. Nisl lorem vulputate
-        neque congue. Adipiscing at diam fringilla arcu turpis vitae aliquet
-        volutpat. Pretium elementum urna ipsum in venenatis eros.
+    <div className="max-w-5xl mx-auto px-4 flex flex-col gap-8">
+      <h2 className="font-semibold text-2xl">{t("introTitle")}</h2>
+      <p className="text-justify tracking-wide leading-10">
+        {t("introDescription")}
       </p>
     </div>
   );
